58f634ce31fa2b1da9ad9999495481f785d2c81b
Vivek <svivek1507@gmail.com>
2023-09-21 11:57:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII219
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = 'A';
+            Console.WriteLine(Convert.ToInt64(first));
+            Console.ReadLine();
+
+        }
+    }
+}
